4962bc748b93bf555db04bed4295f1cc30a1bb1e
packocrayons <gibson.brydon@gmail.com>
2016-10-19 15:07:57 -0400

    Moved everything down the tree - added a root folder

---
+++ tftp17/src/tftp/FileType.java
@@ -0,0 +1,19 @@
+package tftp;
+
+/**
+ * FileType enum used for the file mode type
+ * @author Team 15
+ */
+public enum FileType {
+	OCTET,NETASCII;
+	
+	public static FileType fromString(String str){
+		if(OCTET.name().equalsIgnoreCase(str)){
+			return OCTET;
+		}else if(NETASCII.name().equalsIgnoreCase(str)){
+			return NETASCII;
+		}else{
+			throw new IllegalArgumentException(str+" is not a valid file type");
+		}
+	}
+}
