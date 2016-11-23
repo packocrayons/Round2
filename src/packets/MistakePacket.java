0f6509f34127271318f5bdbebc3126487a363f8e
xlirate <xlirate@live.ca>
2016-11-23 16:46:16 -0500

    Added funtionality to MistakePacket
    It now allows for the creation of packets through succsesive
    concatinates.

--- src/packets/MistakePacket.java
+++ src/packets/MistakePacket.java
@@ -13,6 +13,16 @@ public class MistakePacket extends Packet {
 	private final byte[] bytes;
 	private final String message;
 	
+	public MistakePacket(){
+		bytes = new byte[0];
+		message = "";
+	}
+	
+	private MistakePacket(byte[] data){
+		bytes = data.clone();
+		message = "";
+	}
+	
 	protected MistakePacket(byte[] data, int length,String message){
 		this.bytes = Arrays.copyOf(data, length);
 		this.message=message;
@@ -30,7 +40,23 @@ public class MistakePacket extends Packet {
 	public PacketType getType() {
 		return type;
 	}
-
+	
+	public MistakePacket cat(byte b){
+		return this.cat(new byte[]{b});
+	}
+	
+	public MistakePacket cat(String s){
+		return this.cat(s.getBytes());
+	}
+	
+	public MistakePacket cat(byte[] b){
+		byte[] newData = new byte[bytes.length+b.length];
+		System.arraycopy(bytes, 0, newData, 0, bytes.length);
+		System.arraycopy(b, 0, newData, bytes.length, b.length);
+		return new MistakePacket(newData);
+	}
+	
+	
 	//@Override
 	public static int getBufferSize() {
 		return 1024;//who cares
