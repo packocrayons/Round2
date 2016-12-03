335c54d30d3cdced48d5898d8a6e55f15474f1ae
xlirate <xlirate@live.ca>
2016-12-03 16:54:09 -0500

    Added Test files and a test file generator

---
+++ src/MakeTestFiles.java
@@ -0,0 +1,59 @@
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.junit.Test;
+
+import tftp.FileFactory;
+
+public class MakeTestFiles {
+
+	private static final long[]   fileSizes = {
+			0,
+			1,
+			512,
+			513,
+			127*512,
+			128*512,
+			129*512,
+			(((1<<16)-1)*512),
+			((1<<16)*512),
+			(((1<<16)+1)*512),
+			1<<17*512};
+	private static final String[] fileNames = {
+			"0.txt",
+			"1.txt",
+			"512.txt",
+			"513.txt",
+			"127x512.txt",
+			"128x512.txt",
+			"129x512.txt",
+			"2^16-1x512.txt",
+			"2^16x512.txt",
+			"2^16+1x512.txt",
+			"2^17.txt"};
+	
+	private void write(long size, OutputStream out) throws IOException{
+		int charValue = 0;
+		while(--size>=0){
+			if(size >0){
+				charValue = (charValue+1)%26;
+				out.write(charValue+'a');
+			}else{
+				out.write('\n');
+			}
+		}
+		out.close();
+	}
+	
+	@Test
+	public void test() throws IllegalAccessException, IOException {
+		FileFactory fFac = new FileFactory(".\\testFiles");
+		for(int i = 0; i<Math.min(fileSizes.length, fileNames.length); i++){
+			System.out.println(i);
+			write(fileSizes[i], fFac.writeFile(fileNames[i]));
+		}
+	}
+
+}
--- src/tftp/Client.java
+++ src/tftp/Client.java
@@ -1,7 +1,6 @@
 package tftp;
 
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.DatagramPacket;
